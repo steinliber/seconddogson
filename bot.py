116e0a0d44a26080552b9a6ac99add57c27da250
stein <test@q.com>
2016-12-16 12:22:57 +0800

    新加图

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from chat import deepThought
+from os import listdir
+from os.path import isfile, join
 import logging
+import random
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def echo(bot, update):
     return_msg = str(deepThought.get_response(update.message.text))
     if return_msg == '全然不懂你在说什么':
-        bot.sendPhoto(chat_id=update.message.chat_id, photo=open('static/cat.jpg', 'rb'))
+        catfiles = [f for f in listdir('static/cat') if isfile(join('static/cat', f))]
+        picture = random.choice(catfiles)
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=open(picture, 'rb'))
     bot.sendMessage(chat_id=update.message.chat_id, text=return_msg)
 
 
