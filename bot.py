fd08e1ab424c73ee16b6aad5aca4ee6db8674c5e
Your Name <you@example.com>
2016-12-16 13:05:05 +0900

    test

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from chat import deepThought
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-echo_handler = MessageHandler(Filters.text, echo)
 
 def echo(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+    return_msg = str(deepThought.get_response(update.message.text))
+    if return_msg == 'NO!':
+        print(dir(bot))
+    bot.sendMessage(chat_id=update.message.chat_id, text=return_msg)
 
 
 def start(bot, update):
@@ -15,6 +19,7 @@ def weather(bot, update):
     update.message.reply_text(u'欢饮欢迎')
 
 
+echo_handler = MessageHandler(Filters.text, echo)
 
 updater = Updater('318248420:AAHgJyTmE7FUJQdxbGnd2ERoDdg6IVfjyy4')
 updater.dispatcher.add_handler(CommandHandler('weather', weather))
